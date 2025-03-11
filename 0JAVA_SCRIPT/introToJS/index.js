73b4ab52ce899229d18e03634a39041032debeb7
Monkey D. Luffy <144514182+PankajGajpalla@users.noreply.github.com>
2025-03-11 15:14:53 +0530

    Add files via upload

---
+++ 0JAVA_SCRIPT/introToJS/index.js
@@ -0,0 +1,58 @@
+console.log("Namaskaram!");
+
+// lec2
+
+// var age = 25;
+
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// console.log(age);
+// solve();
+
+// var can be redeclare, can be changed and use
+// var x = 10;
+// var x = 20;
+
+
+// now let and const
+
+// {
+//     let a = 10;
+    // console.log(a);
+// }
+// console.log(a); error cant use out side the block
+
+// var is mutable and it can be redefined
+// let is mutable but cant redefine
+// const is imuatable and cant redefien 
+
+//dynamic datatype
+
+// let a = 10;
+// a = "pankaj";
+// a = true;
+// a = null;
+
+
+// const a  = 29;
+// console.log(a);
+
+// const a = 20;
+// console.log(a);
+
+// let marks = 20;
+// marks = "Pankaj"
+// marks = true;
+// console.log(marks);
+
+let marks = 9038590485093858098304512043529058903405893589438503439058039493058
+console.log(marks);
+
+marks = null;
+console.log(typeof(marks));
